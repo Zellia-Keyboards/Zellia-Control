b6442c1debea1101454843978e89cb72330066b1
stllok <cl20103B11@yahoo.com.hk>
2025-05-24 04:52:56 +0800

    fix(layout): scale up the keyboard size

--- src/lib/Zellia80HE.svelte
+++ src/lib/Zellia80HE.svelte
@@ -17,12 +17,12 @@
 <div class="p-4 bg-white rounded-xl shadow-md inline-block">
     {#each KEYBOARD_LAYOUT as row, y}
         <div
-            class="flex first:*:[&:nth-child(4n+2)]:ml-3 *:nth-15:ml-3 nth-[5]:*:nth-13:ml-14 nth-[6]:*:nth-8:ml-2 nth-[2]:mt-2"
+            class="flex first:*:[&:nth-child(4n+2)]:ml-3.5 *:nth-15:ml-4 nth-[5]:*:nth-13:ml-18 nth-[6]:*:nth-8:ml-4 nth-[2]:mt-4"
         >   
             {#each row as width, x}
                 <button
-                    class="h-12 bg-gray-400 border border-gray-300 rounded-lg items-center justify-center hover:bg-gray-300 hover:cursor-pointer"
-                    style:width="{width * 3}rem"
+                    class="h-14 bg-gray-400 border border-gray-300 rounded-lg items-center justify-center hover:bg-gray-300 hover:cursor-pointer"
+                    style:width="{width * 3.5}rem"
                     onclick={(_) => onClick(x, y, _)}
                 >
                     {values.at(y)?.at(x)?.toString() ?? "null"}
