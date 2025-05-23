8242c9eb49929c3b7bc113236e9ad28849372c28
mhdimo <akahito@naver.com>
2025-05-24 00:36:15 +0200

    fix(keyboard): update key display values for consistency

--- src/lib/KeyboardState.svelte.ts
+++ src/lib/KeyboardState.svelte.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 const KeyboardDisplayValues = writable([
-    ['ESC', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'PRT', 'SCR', 'PAU', 'DEL'],
+    ['ESC', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'PSC', 'SLK', 'PAU'],
     ['`\n~', '1\n!', '2\n@', '#\n3', '4\n$', '5\n%', '6\n^', '7\n&', '8\n*', '9\n(', '0\n)', '-\n_', '=\n+', 'BACKSPACE', 'INS', 'HOM', 'PGU'],
     ['TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[\n{', ']\n}', '\\\n|', 'DEL', 'END', 'PGD'],
     ['CAPS', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';\n:', "'\n\"", 'ENTER'],
