a51e5beefa37e36548c78479a23f21f1b2b4e43d
stllok <cl20103B11@yahoo.com.hk>
2025-06-06 18:20:28 +0800

    feat: refactor on keyboard component

---
+++ src/lib/NewZellia80HE.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { darkMode } from "$lib/DarkModeStore.svelte";
+    import type { Snippet } from "svelte";
+
+    const KEYBOARD_LAYOUT = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.5, 1, 1, 1],
+        [1.75, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.29],
+        [2.25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.82, 1],
+        [1.5, 1, 1.5, 7, 1.6, 1, 1.5, 1, 1, 1],
+    ];
+
+    interface Props {
+        onClick: (x: number, y: number, event: MouseEvent) => void;
+        body: Snippet<[number, number]>;
+    }
+    const { onClick, body }: Props = $props();
+</script>
+
+<div
+    class="p-4 {$darkMode
+        ? 'bg-neutral-800 border border-neutral-700'
+        : 'bg-gray-100 border border-gray-300'} rounded-xl shadow-md inline-block space-y-0.5"
+>
+    {#each KEYBOARD_LAYOUT as row, y}
+        <div
+            class="flex first:*:[&:nth-child(4n+2)]:ml-3.5 *:nth-15:ml-4 nth-[5]:*:nth-13:ml-18.5 nth-[6]:*:nth-8:ml-4 nth-[2]:mt-4 gap-x-0.5"
+        >
+            {#each row as width, x}
+                <button
+                    class="h-14 {$darkMode
+                        ? 'bg-black border-gray-700 hover:bg-gray-800'
+                        : 'bg-gray-50 border-gray-400 hover:bg-gray-100'} border rounded-lg flex flex-col items-center justify-center hover:cursor-pointer gap-1 font-sans"
+                    style:width={y === 5 && x === 3
+                        ? "400px"
+                        : `${width * 3.5}rem`}
+                    onclick={(_) => onClick(x, y, _)}
+                >
+                    {@render body(x, y)}
+                </button>
+            {/each}
+        </div>
+    {/each}
+</div>
