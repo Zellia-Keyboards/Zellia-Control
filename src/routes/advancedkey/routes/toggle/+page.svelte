732a72abcb7d87b49d3695aadc5912e3814e5995
mhdimo <akahito@naver.com>
2025-06-16 12:35:08 +0200

    feat: bind current selected key to NewZellia80He component for improved interaction

--- src/routes/advancedkey/routes/toggle/+page.svelte
+++ src/routes/advancedkey/routes/toggle/+page.svelte
@@ -127,6 +127,7 @@
   onClick={(x, y, event) => {
     console.log(`Key clicked at (${x}, ${y})`, event);
   }}
+  bind:currentSelectedKey={CurrentSelected}
 >
   {#snippet body(x, y)}
   {/snippet}
