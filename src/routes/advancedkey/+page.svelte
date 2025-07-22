72615792ca3b3406ef67bde6a434d26e5048ffce
stllok <cl20103B11@yahoo.com.hk>
2025-07-22 13:54:56 +0800

    fix: missing keyboards' key

--- src/routes/advancedkey/+page.svelte
+++ src/routes/advancedkey/+page.svelte
@@ -81,7 +81,11 @@
   }}
   bind:currentSelectedKey
 >
-  {#snippet body(x, y)}{/snippet}
+  {#snippet body(x, y)}
+  <div       class="hover:scale-90 transition-all duration-300 h-14 {$darkMode
+        ? 'bg-black border-gray-700'
+        : 'bg-gray-50 border-gray-400'} data-[selected=true]:bg-gray-500 data-[selected=true]:border-gray-700 data-[selected=true]:border-4 border rounded-lg flex flex-col items-center justify-center hover:cursor-pointer gap-1 font-sans text-white"
+></div>{/snippet}
 </NewZellia80He>
 <div
   class="rounded-2xl shadow p-8 mt-2 mb-4 grow {$glassmorphismMode
