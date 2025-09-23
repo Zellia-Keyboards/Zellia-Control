450ef771446f267ebca8d27e34ef071dc55a3f7d
mhdimo <akahito@naver.com>
2025-09-23 10:58:55 +0200

    momentarily remove controller codes

--- src-controller/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./interface"
-export * from "./controllers/trinity_pad_controller/controller"
-export * from "./controllers/oholeo_keyboard_controller/controller"
-export * from "./controllers/zellia_80_controller/controller"
-export * from "./controllers/ansi_104_sample_controller/controller"
-export * from "./controllers/destrez_asural_left_controller/controller";
-export * from "./controllers/destrez_asural_right_controller/controller";
